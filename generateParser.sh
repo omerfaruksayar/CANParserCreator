1232558de8d7bb373ce20c7ad5334e8fa6e9b237
omerfaruksayar <omerfsayar@gmail.com>
2022-07-04 13:10:32 +0300

    ROS package creator script

---
+++ generateParser.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+db_name=$1
+dbc_path=$2
+msg_name=$3
+pckg_path=$4
+sbs_topic=$5
+pbs_topic=$6
+
+cd $pckg_path
+catkin_create_pkg $db_name std_msgs roscpp can_msgs message_generation
+cd $db_name/include
+rm -rf $db_name
+cd ../src
+touch parser.cpp
+cantools generate_c_source --database-name $db_name $dbc_path
+mv $db_name.c $db_name.cpp
+mv $db_name.h ../include
+cd ..
+mkdir msg
+cd msg
+touch $msg_name.msg
+cd ..
+sed -i '62 i \  <exec_depend>message_runtime</exec_depend>' package.xml
+sed -i '17,209d' CMakeLists.txt
+echo 'add_message_files(
+   FILES
+   '$msg_name'.msg
+ )' >> CMakeLists.txt
+
+ echo "generate_messages(
+   DEPENDENCIES
+   can_msgs
+   std_msgs
+ )" >> CMakeLists.txt
+
+ echo "catkin_package(
+  INCLUDE_DIRS include
+  LIBRARIES cantool
+  CATKIN_DEPENDS can_msgs roscpp std_msgs message_runtime
+#  DEPENDS system_lib
+)" >> CMakeLists.txt
+
+echo 'include_directories(
+  include
+  ${catkin_INCLUDE_DIRS}
+)' >> CMakeLists.txt
+
+echo 'add_library(${PROJECT_NAME}
+   src/'$db_name'.cpp
+ )' >> CMakeLists.txt
+
+echo 'add_executable('$db_name'_node src/parser.cpp src/'$db_name'.cpp)' >> CMakeLists.txt
+
+echo 'add_dependencies('$db_name'_node ${${PROJECT_NAME}_EXPORTED_TARGETS} ${catkin_EXPORTED_TARGETS})' >> CMakeLists.txt
+
+echo 'target_link_libraries('$db_name'_node
+   ${catkin_LIBRARIES}
+ )' >> CMakeLists.txt
+
+echo 'install(TARGETS ${PROJECT_NAME}
+   ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+   LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+   RUNTIME DESTINATION ${CATKIN_GLOBAL_BIN_DESTINATION}
+ )' >> CMakeLists.txt
+
+echo 'install(DIRECTORY include/${PROJECT_NAME}/
+   DESTINATION ${CATKIN_PACKAGE_INCLUDE_DESTINATION}
+   FILES_MATCHING PATTERN "*.h"
+   PATTERN ".svn" EXCLUDE
+ )' >> CMakeLists.txt
